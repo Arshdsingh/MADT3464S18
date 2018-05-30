5cd91fd26c2cf8e5a6be78c21f7fdbcba159e499
Arshdeep singh <arshdeepsingh.gni@gmail.com>
2018-05-30 16:45:21 -0400

    Add files via upload

---
+++ Day_01.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day_01;
+
+/**
+ *
+ * @author macstudent
+ */
+public class Day_01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        int a;
+       String str="hello";
+       char b = 'a';
+       float f = 27.3f;
+       int n=20;
+       int n1 = 0;
+       n1 *= 2;
+       System.out.print(n1);
+       System.out.print("Hello Java");
+        System.out.print("Hello Java"+f);
+         System.out.print("Hello Java"+n1);
+    }
+    
+}
